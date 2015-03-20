1aa622b8fb26eb6bae937fbe43d26e8a2b333a74
Ivan Arcuschin <iarcuschin@gmail.com>
2015-03-20 16:13:34 -0300

    Hanoi

--- hanoi.py
+++ hanoi.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import sys
 import os
+import time
+
 if len(sys.argv) > 2:
     print "Too many arguments"
     sys.exit()
@@ -12,10 +14,10 @@ else:
 
 # helper functions
 def display():
-#    os.system('clear')
-    print "T1: ", towers[0]
-    print "T2: ", towers[1]
-    print "T3: ", towers[2]
+    os.system('clear')
+    print "T1: ", list(reversed(towers[0]))
+    print "T2: ", list(reversed(towers[1]))
+    print "T3: ", list(reversed(towers[2]))
 
 def attemptMove(i):
     destination = []
@@ -24,7 +26,6 @@ def attemptMove(i):
 
     for j in xrange(3):
         if abs(j-i) == 1 and (len(towers[j]) == 0 or towers[i][0] < towers[j][0]):
-            print i,j
             destination.append(j)
     return destination
 
@@ -42,24 +43,20 @@ while len(towers[2]) != n:
     # check which tower should move next
     origin_tower = -1
     destination_tower = -1
-    for i in xrange(3):
-        print "i:",i
-        possible_destinations = attemptMove(i)
+    for origin_tower in xrange(3):
+        possible_destinations = attemptMove(origin_tower)
         if len(possible_destinations) == 0:
             continue
         found = False
         for destination_tower in possible_destinations:
             if not (destination_tower == last_origin_tower and origin_tower == last_destination_tower):
-                last_origin_tower = i
+                last_origin_tower = origin_tower
                 last_destination_tower = destination_tower
-                origin_tower = i
                 found = True
                 break
         if found:
             break
     #print next_tower
-    print "asdf", origin_tower, destination_tower
-    #print last_origin_tower, last_destination_tower
     disc = towers[origin_tower][0]
     towers[origin_tower] = towers[origin_tower][1:]
     if origin_tower < destination_tower:
@@ -67,4 +64,5 @@ while len(towers[2]) != n:
     else:
         towers[origin_tower - 1][:0] = [disc]
     display()
+    #time.sleep(0.25) # sleep 250 milliseconds
     a = raw_input('')
